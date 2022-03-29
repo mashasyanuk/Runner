e72a0e7947cb851e1941e2f0cc6a5408bc752aed
m yan <61414925+mashasyanuk@users.noreply.github.com>
2022-03-29 14:44:56 +0300

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class PlayerController : MonoBehaviour {
+public float speed = 6.0f;
+[SerializeField] float deltaZ;
+public float gravity = -9.8f;
+private CharacterController _charController;
+void Start() {
+_charController = GetComponent<CharacterController>();
+}
+void Update() {
+float deltaX = Input.GetAxis("Horizontal") * speed;
+
+Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+movement = Vector3.ClampMagnitude(movement, speed);
+movement.y = gravity;
+movement *= Time.deltaTime;
+movement = transform.TransformDirection(movement);
+_charController.Move(movement);
+}
+}
